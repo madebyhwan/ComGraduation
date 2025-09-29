7458c1f7229b5b3ce4c49b48620c5f4c1c028ca4
DUCK <duck@DUCKs-MacBook-Pro.local>
2025-09-29 20:22:19 +0900

    DB 속성 정리 및 연결

---
+++ server/models/lectures.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const lecturesSchema = new mongoose.Schema({
+  lectName:{
+    type: String,
+    required: true, // 필수 항목
+    trim: true      // 앞뒤 공백 제거
+  },
+  lectCode:{  // 사용자의 직접 입력을 위해 required 삭제
+    type: String,
+    match: /^[A-Z]{4}[0-9]{4}$/,
+    trim: true
+  },
+  lectDiv:{   // 사용자의 직접 입력을 위해 required 삭제
+    type: String,
+    match: /^[0-9]{3}$/,
+    trim: true
+  },
+  lectCredit:{
+    type: Number,
+    required: true,
+    min: 1
+  },
+  lectYear:{  // 사용자의 직접 입력을 위해 required 삭제
+    type: Number,
+    enum: [2021, 2022, 2023, 2024, 2025]
+  },
+  lectSemester:{  // 사용자의 직접 입력을 위해 required 삭제
+    type: String,
+    enum: ['1학기', '2학기', '계절학기(하계)', '계절학기(동계)']
+  },
+  lectProf:{
+    type: String,
+    trim: true
+  },
+  lectTime:{
+    type: String
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('lectures', lecturesSchema, 'lectures');
